95e9aa35b6e8d59a79de14cfbc6f4fa4ce0683c6
medoo0 <raslanmohamed156@yahoo.com>
2020-08-26 20:55:04 +0200

    done

--- Documents/workspace-sts-3.9.12.RELEASE/full-product/src/main/java/com/app/product/fullproduct/FullProductApplication.java
+++ Documents/workspace-sts-3.9.12.RELEASE/full-product/src/main/java/com/app/product/fullproduct/FullProductApplication.java
@@ -3,6 +3,9 @@ package com.app.product.fullproduct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// Enty Point of App
+
 @SpringBootApplication
 public class FullProductApplication {
 
